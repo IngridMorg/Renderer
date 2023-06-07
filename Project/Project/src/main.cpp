0c6b8b83bb882969aa439520de04f24eb87a95a4
IngridMorg <sc20iecm@leeds.ac.uk>
2023-06-07 19:44:59 +0100

    can now close window with escape key

--- Project/Project/src/main.cpp
+++ Project/Project/src/main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+#include <glad.h>
 #include <GLFW/glfw3.h>
+
+//function prototypes
+//create a callback for window size so we can resize the window easily
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+//input handler
+void processInput(GLFWwindow* window);
 
 int main(void)
 {
@@ -20,11 +27,26 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    
+    glViewport(0, 0, 800, 600);
+    //set framebuffer size callback
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        //process events
+        processInput(window);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -35,4 +57,17 @@ int main(void)
 
     glfwTerminate();
     return 0;
+}
+
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
 }
